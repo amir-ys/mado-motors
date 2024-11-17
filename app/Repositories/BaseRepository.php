52e176d9bb7f7266044ba9f98d8e3ad19a4b6fd7
amir-ys <amir.you74@gmail.com>
2024-11-18 01:14:45 +0330

    feat: add l5 Repository packages

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\BaseRepositoryInterface;
+use Prettus\Repository\Eloquent\BaseRepository as L5Repository;
+
+abstract class BaseRepository extends L5Repository implements BaseRepositoryInterface
+{
+}
